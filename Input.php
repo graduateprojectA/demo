8d7f4d904c06cd45628e60c19626e2ea550e4918
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2020-11-25 01:46:26 +0900

    input_first

---
+++ Input.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta charset="UTF-8">
+<style type="text/css">
+
+</style>
+<body>
+  <div class="login_box">
+        <form method="POST" action="Input_back.php">
+            학년을 입력해주세요
+            <br/>
+            <input value = "학번"/>
+            <br/>
+            이번 학기에 들을 학점을 입력해주세요
+            <br/>
+            <input value = "학점수"/>
+            <br/>
+            <br><br>
+            <a href = "#timetable1">
+            다음
+            </a> 
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <a name = "timetable1"></a>
+            시간표선택
+            <div>
+                <label><input type="checkbox" value="blue" style = "margin-left:0px; margin-right:0px; margin-top:0px; margin-bottom:0px;"></label>
+                <label><input type="checkbox" value="red"></label>
+            </div>        
+
+            <a href = "#timetable2">
+            다음
+            </a> 
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <a name = "timetable2"></a>
+            과목선택
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br>
+            <input type = "submit"/>
+      </form>
+  </div>
+</body>
+</html> 
