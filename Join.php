08d5b973d42239d059e1012e102ed4082faeb9c0
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2020-11-25 13:43:36 +0900

    modal

--- Join.php
+++ Join.php
@@ -1,3 +1,11 @@
+<?php session_start();
+$id = $_SESSION['user_id'];
+$name = $_SESSION['user_name'];
+$email = $_SESSION['user_email']; 
+$password = $_SESSION['user_pw'];
+$conn = mysqli_connect('localhost', 'root', '1234', 'graduate');
+?>
+
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -25,37 +33,109 @@
   height:5vh;
   margin: 0.5%;
   border:0;
+  background-color:white;
+  text-align:left;
+  padding-left:0px;
 }
 input::placeholder {
   color: #99aa8c;
 }
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+    
+.modal-content {
+  background-color: #f0f0f0;
+  margin: 20% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 40%; /* Could be more or less, depending on screen size */     
+  border-radius: 10px;    
+  height:40%;                 
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
 </style>
 
 <body>
   <div class="default_box">
-        <form method="POST" action="Join_back.php">
+        <!-- <form method="POST" action="Join_back.php"> -->
           <input type="text" class="txt_box" id="ex4-id-input" name="id" placeholder="학번 7자리"/>
           <br/>
           <input type="password" class="txt_box" id="ex4-pw-input" name="password" placeholder="비밀번호"/>
           <br/>
-          <input type="text" class="txt_box" id="ex4-major-input" name="major" placeholder="전공"/>
+          <button id="myBtn" class = "txt_box">전공 선택</button>
+
+          <div id="myModal" class="modal">
+            <div class="modal-content">
+              <span class="close">&times;</span>                                                               
+              <p>전공을 검색하세요</p>
+              <input id = "majors" placeholder = "컴퓨터공학전공">
+              <button>선택</button>
+            </div>
+          </div>
+
           <br/>
           <input type="submit" value="회원가입" class="default_btn">
           <br><br>
           <a href = "Login.php" style = "font-size: 18px; color: #777777; 
           text-decoration: none;">이미 계정이 있으신가요? <br>
           지금 <p style="display:inline; color:#99aa8c; font-weight:bold;">로그인</p>하세요!</a>  
-      </form>
-  </div>
+      <!-- </form> -->
 
+</div>
+</body>
 
-
-
-
-
-
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script type="text/javascript">
+
+// 자동검색
+$(function() {
+  var allMajors = [];
+  <?php 
+    $major_ = "SELECT major_name FROM major_name";
+    $major = mysqli_query($conn, $major_);
+    while($result = mysqli_fetch_assoc($major)){?>
+      allMajors.push('<?php echo $result['major_name'] ?>');
+  <?php }  ?>
+
+  allMajors.push('컴퓨터공학전공');
+  $("#majors").autocomplete({
+    source: allMajors,
+    select: function(event, ui) {
+        console.log(ui.item);
+    },
+    focus: function(event, ui) {
+        return false;
+        //event.preventDefault();
+    } 
+  });
+});
+
 (function cleanText($) {
   var $pw = $('#ex4-id-input, #ex4-pw-input,ex4-major-input')
   $pw.on('click focus', function() {
@@ -68,6 +148,31 @@ input::placeholder {
     }
   });
 })(jQuery);
+
+var modal = document.getElementById('myModal');
+ // Get the button that opens the modal
+ var btn = document.getElementById("myBtn");
+ // Get the <span> element that closes the modal
+ var span = document.getElementsByClassName("close")[0];                                          
+ // When the user clicks on the button, open the modal 
+ btn.onclick = function() {
+     modal.style.display = "block";
+ }
+ // When the user clicks on <span> (x), close the modal
+ span.onclick = function() {
+     modal.style.display = "none";
+ }
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function(event) {
+     if (event.target == modal) {
+         modal.style.display = "none";
+     }
+ }
+
+
+
 </script>
-</body>
+
 </html> 
+
+
